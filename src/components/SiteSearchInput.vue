697e1db7f94894c9856a9fa09228163fd61b7679
Oleh Kozhushko <oleh.kozhushko@otakoyi.com.ua>
2021-05-29 17:25:05 +0300

    update cards view with header and menu

---
+++ src/components/SiteSearchInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="search-input">
+    <input
+      type="text"
+      placeholder="Пошук по сайту"
+      @input="$emit('input', $event.target.value)"
+      :value="value"
+    />
+    <button>Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteSearchInput',
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.search-input {
+  background: #fff;
+  padding-left: 0.8rem;
+  padding-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  display: flex;
+}
+
+.search-input input {
+  flex-grow: 1;
+  border: none;
+}
+
+input:focus {
+  outline: none;
+  border: none;
+}
+
+input::placeholder {
+  font-weight: 600;
+  font-size: 12px;
+  color: #333;
+  text-transform: uppercase;
+}
+
+.search-input button {
+  background: #b30c1d;
+  border-radius: 2px;
+  border: 3px solid #fff;
+  padding: 0.5rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 12px;
+}
+</style>
--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -1,20 +1,78 @@
 <template>
   <div>
-    <h1>Some cards</h1>
+    <header :style="{ background: opened ? 'dodgerblue' : '' }">
+      <button @click="toggle">{{ opened ? 'Close' : 'Open' }}</button>
+      <div class="logo">Logo</div>
+      <h1>My Site</h1>
+      <section class="menu" v-if="opened">
+        <SiteSearchInput style="margin: 15px 0" v-model="search" />
+        <h2>{{ search || 'Some content is here' }}</h2>
+      </section>
+    </header>
   </div>
 </template>
 
 <script>
+import SiteSearchInput from '@/components/SiteSearchInput.vue';
 export default {
   name: 'Cards',
+
+  components: {
+    SiteSearchInput,
+  },
+
+  data() {
+    return {
+      opened: false,
+      search: '',
+    };
+  },
+
+  methods: {
+    toggle() {
+      this.opened = !this.opened;
+    },
+  },
 };
 </script>
 
 <style scoped>
-h1 {
-  font-size: 32px;
-  color: orange;
-  margin-top: 4rem;
-  margin-left: 4rem;
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  height: 64px;
+  background: orange;
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  font-size: 60px;
+  font-weight: 700;
+  color: #fff;
+}
+
+.menu {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background: dodgerblue;
+  height: 400px;
+  padding: 0 3rem;
+}
+.menu h2 {
+  color: #fff;
+  font-size: 54px;
+  text-align: center;
+  margin-top: 3rem;
 }
 </style>
